f0311bfc9a37d3d0c2ae17e0927b5ecd16c264d3
Ajose Tosin <tosinemidastouch@gmail.com>
2021-01-01 16:41:12 +0100

    removal of active class

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-
+        panel.classList.add('active')
     })
 });
 
-
+const removeActiveClasses = () => {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    });
+}
 
 
 
