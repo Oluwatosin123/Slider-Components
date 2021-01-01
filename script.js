20b3f63d1d1e065313767fc4c54eda2e60d47c64
Ajose Tosin <tosinemidastouch@gmail.com>
2021-01-01 16:16:40 +0100

    adding javascript to the code

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+
     })
 });
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const panels = document.querySelectorAll('.panel')
+
+// panels.forEach(panel => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// });
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
